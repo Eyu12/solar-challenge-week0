ed32aa0046f729add9b6fbc8c3a701550d5c7321
Eya Zewdu <eyazew1@gmail.com>
2025-11-10 17:58:00 +0300

    [SRC] Add DataLoader classe

---
+++ src/data_loader.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+class DataLoader:
+    """Load CSV data files."""
+    
+    def __init__(self, filepath):
+        self.filepath = filepath
+    
+    def load_csv(self):
+        return pd.read_csv(self.filepath)
+    
+    def load_excel(self):
+        return pd.read_excel(self.filepath)
